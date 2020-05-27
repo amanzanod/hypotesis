21e7dc0ee7e623ee38cbd88aa05b8b49978f9300
Antonio Manzano <amanzanod@uoc.edu>
2020-05-27 23:04:08 +0200

    crear containers item y enrol

---
+++ hypotesis_item/deploy_django.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "*** EXECUTING DJANGO HYPOTESIS ITEM ... ***"
+echo "Wait for DB ..."
+sleep 0m 15s
+echo "15s"
+sleep 0m 15s
+echo "30s"
+sleep 0m 15s
+echo "45s"
+echo "Running ..."
+# Collect static files
+#echo "Collect static files"
+#python manage.py collectstatic --noinput
+# Apply database migrations
+echo "Apply database migrations..."
+python manage.py migrate --noinput
+# Start server
+echo "Starting server..."
+python manage.py runserver "$1"
+echo "*** EXECUTING DJANGO END > ***"
